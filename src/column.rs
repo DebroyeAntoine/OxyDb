090822953b17b7964b83cdaf3d9411c78eead760
Antoine Debroye <ant.debroye@gmail.com>
2026-03-01 15:50:46 +0100

    column: remove unused remove function
    As rows are deleted by compaction now, the old method is no more used.

--- src/column.rs
+++ src/column.rs
@@ -141,31 +141,6 @@ impl Column {
         }
     }
 
-    /// Remove the value at the specified row index.
-    ///
-    /// Returns `Ok(())` if the index is valid or an `Err(String)` if it is out of bounds
-    pub fn remove(&mut self, row_idx: usize) -> Result<(), String> {
-        if self.len() <= row_idx {
-            return Err("The row index is too high".into());
-        }
-        match &mut self.data {
-            ColumnData::Int(col) => {
-                col.remove(row_idx);
-            }
-            ColumnData::Text(col) => {
-                col.remove(row_idx);
-            }
-            ColumnData::Bool(col) => {
-                col.remove(row_idx);
-            }
-            ColumnData::Float(col) => {
-                col.remove(row_idx);
-            }
-        }
-        self.null_bitmap.remove(row_idx);
-        Ok(())
-    }
-
     /// Physically compacts the column by applying a deletion bitmap.
     ///
     /// # Parameters
@@ -397,24 +372,6 @@ mod tests {
             assert!(col.get(i).unwrap().is_null());
             assert!(col.null_bitmap[i]);
         }
-    }
-
-    #[test]
-    fn test_delete() {
-        let mut col = Column::new("test".into(), DataType::Int);
-
-        col.push(Value::Int(42)).unwrap();
-        col.push(Value::Int(59)).unwrap();
-        col.push(Value::Null).unwrap();
-
-        assert_eq!(col.len(), 3);
-
-        col.remove(1).unwrap();
-        assert_eq!(col.len(), 2);
-        assert_eq!(col.get(0), Some(Value::Int(42)));
-        assert!(!col.null_bitmap[0]);
-        assert_eq!(col.get(1), Some(Value::Null));
-        assert!(col.null_bitmap[1]);
     }
 
     #[test]
